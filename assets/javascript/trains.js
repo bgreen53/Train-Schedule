e49bd8e2f66b8677780fe6cc31ab664b2ff6192c
bgreen53 <green.barry53@gmail.com>
2020-03-17 16:18:41 -0700

    added css reset, firebase, html skeleton

---
+++ assets/javascript/trains.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyABvv4dF_DgdgDDQXAQh4SAk0z4Hpgn02Q",
+    authDomain: "train-schedule-8ec29.firebaseapp.com",
+    databaseURL: "https://train-schedule-8ec29.firebaseio.com",
+    projectId: "train-schedule-8ec29",
+    storageBucket: "train-schedule-8ec29.appspot.com",
+    messagingSenderId: "170223552655",
+    appId: "1:170223552655:web:7b788bfbc4c613827348fa"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
